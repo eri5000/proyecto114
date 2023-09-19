283162b3bf0b87f10f174861807c9c58514ca478
eri5000 <107961194+eri5000@users.noreply.github.com>
2023-09-19 16:10:01 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-
+Nose_X=0;
+Nose_Y=0;
 
 function preload(){
-
+ gorro=loadImage('https://i.postimg.cc/cJWnsRCr/gorro.png');
 }
 
 function setup(){
@@ -10,12 +11,24 @@ function setup(){
     video = createCapture(VIDEO);
     video.size(300,300);
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 
     
+}
+function gotPoses(results){
+    if(results.length > 0 ){
+     console.log(results);
+     Nose_X=results[0].pose.nose.x-40;
+     Nose_Y=results[0].pose.nose.y-130;
+     console.log("Nose X= " + Nose_X);
+     console.log("Nose Y= " + Nose_Y);
+    }
 }
 
 function draw(){
 image(video , 0 , 0 , 300 , 300);
+image(gorro, Nose_X,Nose_Y,90,90);
 }
 
 function take_snapshot(){
